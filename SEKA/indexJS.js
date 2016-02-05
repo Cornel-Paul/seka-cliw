66c1ebfe355a9e75c43da8465af8891aa3ffddac
Cornel Paul Acatrinei <Cornel Paul Acatrinei>
2016-02-05 16:15:57 +0200

    http request for duckduckgo (totusi n-am reusit sa fac un request cu succes..)

--- SEKA/indexJS.js
+++ SEKA/indexJS.js
@@ -55,15 +55,40 @@ function loadCss(){
 
 function parseURL(){
 	var url = window.location.href;
-	url = url.split("?")
-	var query = url[1].split("=")
-	duckSearch((query[1]))
+	url = url.split("?");
+	var query = url[1].split("=");
+	duckSearch((query[1]));
 }
 
 function duckSearch(query){
-	var queryDuck = "http://api.duckduckgo.com/?q=" + query + "&format=json"
-	alert(queryDuck);
+	var queryDuck = "http://api.duckduckgo.com/?q=" + query;
+	
+	
+	var xmlhttp = new XMLHttpRequest();
+	
+	xmlhttp.open("GET", queryDuck, true);
+	xmlhttp.send();
+	
+	xmlhttp.addEventListener("readystatechange", processRequest(xmlhttp), false);
+	
+	//xmlhttp.onreadystatechange = processRequest(xmlhttp);
+	/*xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var myArr = JSON.parse(xmlhttp.responseText);
+			processRequest(myArr);
+		}
+	};
+	*/
 }
+
+function processRequest(xmlhttp){
+	alert(xmlhttp.readyState)
+	if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+		alert("OKOKOK!");
+	}
+}
+
+
 
 function testResults(form) {
     var TestVar = document.getElementById('sample1').value
